f798670b88090d8dea612dcdd8263e1a42acdacf
rihannatanya <rihanna.tanya@student.moringaschool.com>
2026-06-30 11:28:09 +0300

    Complete JavaScript functions lab

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+// Function 1
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Function 2
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+// Function 3
+function findMaximum(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
+// Function 4
+function isPalindrome(word) {
+  const lowercaseWord = word.toLowerCase();
+  const reversedWord = lowercaseWord.split("").reverse().join("");
 
+  return lowercaseWord === reversedWord;
+}
 
-// This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+// Function 5
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * discountPercentage / 100);
+}
+
+// This is required for the test to function properly
+module.exports = {
+  calculateTax,
+  convertToUpperCase,
+  findMaximum,
+  isPalindrome,
+  calculateDiscountedPrice
+};
